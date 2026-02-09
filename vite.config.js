9a0566fa9c9e144dd69692520be45692e0b7b5aa
vaibhav <vaibhavagarwal277@gmail.com>
2026-02-09 16:42:55 +0530

    fix: corrected vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portfolio_f/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
